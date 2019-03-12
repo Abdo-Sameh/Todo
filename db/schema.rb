fe6103c8c5357ef8b81aa61c24f414ff542ff47a
Abdo-Sameh <abdo.sameh2000@gmail.com>
2019-03-12 15:39:25 +0200

    ability to mark as completed

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_09_213606) do
+ActiveRecord::Schema.define(version: 2019_03_11_205138) do
 
   create_table "todo_items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.bigint "todo_list_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "completed_at"
     t.index ["todo_list_id"], name: "index_todo_items_on_todo_list_id"
   end
 
